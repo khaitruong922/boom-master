258bca90f91909b81c50eb3e499a155fae67f0b6
Khai Truong <kd.kidr@gmail.com>
2021-01-29 16:21:10 +0700

    Smooth 4 directional movement

--- Assets/Scripts/Movement/MoveOnInput.cs
+++ Assets/Scripts/Movement/MoveOnInput.cs
@@ -1,4 +1,3 @@
-  
 #pragma warning disable 0649
 
 using UnityEngine;
@@ -6,13 +5,66 @@ using UnityEngine;
 [RequireComponent(typeof(MoveComponent))]
 public class MoveOnInput : MonoBehaviour
 {
+    [SerializeField]
+    private KeyCode leftKey = KeyCode.A, rightKey = KeyCode.D, upKey = KeyCode.W, downKey = KeyCode.S;
+    private KeyCode latestKey = KeyCode.None;
     private MoveComponent moveComponent;
     private void Awake()
     {
         moveComponent = GetComponent<MoveComponent>();
     }
+    private Vector2 KeyToVector2(KeyCode keyCode)
+    {
+        if (keyCode == leftKey) return new Vector2(-1, 0);
+        if (keyCode == rightKey) return new Vector2(1, 0);
+        if (keyCode == upKey) return new Vector2(0, 1);
+        if (keyCode == downKey) return new Vector2(0, -1);
+        return Vector2.zero;
+    }
+    private void ComputekLatestKey()
+    {
+        if (Input.GetKeyDown(leftKey)) latestKey = leftKey;
+        if (Input.GetKeyDown(rightKey)) latestKey = rightKey;
+        if (Input.GetKeyDown(upKey)) latestKey = upKey;
+        if (Input.GetKeyDown(downKey)) latestKey = downKey;
+    }
     private void Update()
     {
-        moveComponent.Move(new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized);
+        ComputekLatestKey();
+        Vector2 direction = Vector2.zero;
+        int keyCount = 0;
+        if (Input.GetKey(leftKey))
+        {
+            keyCount++;
+            direction += KeyToVector2(leftKey);
+        }
+        if (Input.GetKey(rightKey))
+        {
+            keyCount++;
+            direction += KeyToVector2(rightKey);
+        }
+        if (Input.GetKey(upKey))
+        {
+            keyCount++;
+            direction += KeyToVector2(upKey);
+        }
+        ;
+        if (Input.GetKey(downKey))
+        {
+            keyCount++;
+            direction += KeyToVector2(downKey);
+        }
+
+        if (keyCount == 0)
+        {
+            moveComponent.Move(Vector2.zero);
+            return;
+        }
+        if (keyCount == 1)
+        {
+            moveComponent.Move(direction);
+            return;
+        }
+        moveComponent.Move(KeyToVector2(latestKey));
     }
 }

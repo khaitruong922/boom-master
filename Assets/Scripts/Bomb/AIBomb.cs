ae8fae838de5113ec648c5a85c711d7eaf9edc08
vothanhthong <tthong.vox@gmail.com>
2021-01-31 22:08:02 +0700

    update AIbomb

---
+++ Assets/Scripts/Bomb/AIBomb.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BombPlacer))]
+public class AIBomb : MonoBehaviour
+{
+  [SerializeField] private float radius = 5f;
+  private BombPlacer bombPlacer;
+  private void Awake()
+  {
+    bombPlacer = GetComponent<BombPlacer>();
+  }
+  public void PlaceBomb()
+  {
+    float x = Random.Range(-radius, radius);
+    float y = Random.Range(-radius, radius);
+    bombPlacer.PlaceBomb(new Vector3(x, y) + transform.position);
+  }
+}

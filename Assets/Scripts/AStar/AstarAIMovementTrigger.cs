89ee9a82dc662002a9f6d87a879e81c08d814cc3
Khai Truong <kd.kidr@gmail.com>
2021-02-01 10:32:25 +0700

    AI Movement Trigger

---
+++ Assets/Scripts/AStar/AstarAIMovementTrigger.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+[RequireComponent(typeof(AIPath))]
+public class AstarAIMovementTrigger : MonoBehaviour
+{
+    [SerializeField] private float triggerRange=10;
+    private AIPath aIPath;
+    private void Awake()
+    {
+        aIPath = GetComponent<AIPath>();
+        aIPath.canMove = false;
+    }
+    private void Update()
+    {
+        if (aIPath.remainingDistance > triggerRange) return;
+        aIPath.canMove = true;
+        Destroy(this);
+    }
+}

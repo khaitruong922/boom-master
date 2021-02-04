99174f814580f6da0d77d94f25911d7e28f5acaa
Khai Truong <kd.kidr@gmail.com>
2021-02-04 14:22:47 +0700

    Add new stuff

--- Assets/Scripts/PoolSystem/PoolObjectFactory.cs
+++ Assets/Scripts/PoolSystem/PoolObjectFactory.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 public class PoolObjectFactory
 {
     [SerializeField] private PoolObjectType type;
-    public GameObject Get(Vector3 position)
-    {
-        return PoolManager.Instance.Get(type, position);
-    }
     public GameObject Get(Vector3 position, Quaternion quaternion)
     {
         return PoolManager.Instance.Get(type, position, quaternion);
     }
+    public GameObject Get(Vector3 position)
+    {
+        return Get(position, Quaternion.identity);
+    }
     public void Spawn(Vector3 position)
     {
-        PoolManager.Instance.Get(type, position);
+        Get(position, Quaternion.identity);
     }
 }

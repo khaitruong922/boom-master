b36a29f49885037a30826b6526642dcc008597e4
Khai Truong <kd.kidr@gmail.com>
2021-02-04 11:48:21 +0700

    Fix floating text

--- Assets/Scripts/Movement/TransformAttach.cs
+++ Assets/Scripts/Movement/TransformAttach.cs
@@ -6,9 +6,18 @@ public class TransformAttach : MonoBehaviour
 {
     [Header("Optional")]
     [SerializeField] private Transform attachTransform;
-    public Transform AttachTransform { get => attachTransform; set => attachTransform = value; }    
-    private void LateUpdate() {
-        if (attachTransform!=null) transform.position = attachTransform.position;
+    [SerializeField] private float randomOffset = 1f;
+    [SerializeField] private Vector2 fixedOffset = new Vector2(0f, 1f);
+    public Transform AttachTransform { get => attachTransform; set => attachTransform = value; }
+    private Vector3 offset;
+    private void OnEnable()
+    {
+        offset = (Vector3)fixedOffset + new Vector3(RandomOffset, RandomOffset);
     }
+    private void LateUpdate()
+    {
+        if (attachTransform != null) transform.position = attachTransform.position + offset;
+    }
+    private float RandomOffset => Random.Range(-randomOffset, randomOffset);
 }
 

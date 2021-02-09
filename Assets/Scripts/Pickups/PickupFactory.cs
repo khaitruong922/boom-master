8737543e21a146ef0f16a82ff1400bb280150d47
Khai Truong <kd.kidr@gmail.com>
2021-02-09 16:11:10 +0700

    Pickup rate

--- Assets/Scripts/Pickups/PickupFactory.cs
+++ Assets/Scripts/Pickups/PickupFactory.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[CreateAssetMenu(menuName = "PickupFactory",fileName="PickupFactory")]
+[CreateAssetMenu(menuName = "PickupFactory", fileName = "PickupFactory")]
 public class PickupFactory : ScriptableObject
 {
     [System.Serializable]
@@ -25,16 +25,17 @@ public class PickupFactory : ScriptableObject
         {
             cumulativeWeight += pickupDrops[i].DropWeight;
             dropTable[i] = cumulativeWeight;
+            Debug.Log(cumulativeWeight);
         }
         if (cumulativeWeight > totalDropWeight) Debug.LogWarning($"Cumulative weight is higher than drop weight.");
     }
     public GameObject GetRandomPickup()
     {
         if (dropTable.Length == 0) return null;
-        int randomNumber = Random.Range(0, totalDropWeight);
+        int randomNumber = Random.Range(1, totalDropWeight);
         for (int i = 0; i < dropTable.Length; i++)
         {
-            if (randomNumber < dropTable[i]) return pickupDrops[i].Pickup;
+            if (randomNumber <= dropTable[i]) return pickupDrops[i].Pickup;
         }
         return null;
     }

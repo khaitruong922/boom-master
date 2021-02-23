6a92be00c3f017376507151eb4397b63c52f83d4
Khai Truong <kd.kidr@gmail.com>
2021-02-23 11:25:08 +0700

    Add health potion + Balance drop rate

---
+++ Assets/Scripts/Pickups/HealthPickup.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickup : MonoBehaviour, IPickup
+{
+    [SerializeField] private float healAmount = 20;
+    public void GrantEffect(Player player)
+    {
+        player.GetComponent<Health>()?.Heal(healAmount);
+    }
+}
